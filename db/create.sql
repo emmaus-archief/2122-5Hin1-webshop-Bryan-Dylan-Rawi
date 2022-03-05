710ceb0444e6fb2f5b389857ea8adc3cf0920b58
rawi305em <74540039+rawibidar@users.noreply.github.com>
2022-03-05 23:38:59 +0000

    tabellen afgemaakt, foutjes gecorrigeerd en een heel nieuwe tabel toegevoegd

--- db/create.sql
+++ db/create.sql
@@ -12,8 +12,6 @@ CREATE TABLE products (
   rating_id INTEGER
   );
   
-  
-
   CREATE TABLE review (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   rating VARCHAR(255)
@@ -33,11 +31,53 @@ CREATE TABLE products (
   studio TEXT
   );
 
+  CREATE TABLE age(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  leeftijd VARCHAR(255)
+  );
+
+  CREATE TABLE playtime(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  speeltijd VARCHAR(255)
+  );
+
   insert into review (rating) values ('heel leuk');
   insert into review (rating) values ('leuk');
   insert into review (rating) values ('niet zo leuk');
   insert into review (rating) values ('slecht');
-  
+
+  insert into genres (genre) values ('fps');
+  insert into genres (genre) values ('rpg');
+  insert into genres (genre) values ('mmo');
+  insert into genres (genre) values ('horror');
+  insert into genres (genre) values ('race');
+  insert into genres (genre) values ('sim');
+  insert into genres (genre) values ('survival');
+
+  insert into multi (multiplayer) values ('online');
+  insert into multi (multiplayer) values ('geen online');
+  insert into multi (multiplayer) values ('co-op');
+
+  insert into multi (multiplayer) values ('Bungie');
+  insert into multi (multiplayer) values ('EA');
+  insert into multi (multiplayer) values ('SONY');
+  insert into multi (multiplayer) values ('Ubisoft');
+  insert into multi (multiplayer) values ('FromSoftware');
+  insert into multi (multiplayer) values ('Mojang');
+  insert into multi (multiplayer) values ('Atlus');
+
+  insert into age (leeftijd) values ('3+');
+  insert into age (leeftijd) values ('7+');
+  insert into age (leeftijd) values ('12+');
+  insert into age (leeftijd) values ('16+');
+  insert into age (leeftijd) values ('18+');
+
+  insert into playtime (speeltijd) values ('20 uur+');
+  insert into playtime (speeltijd) values ('40 uur+');
+  insert into playtime (speeltijd) values ('60 uur+');
+  insert into playtime (speeltijd) values ('80 uur+');
+  insert into playtime (speeltijd) values ('100 uur+');
+  insert into playtime (speeltijd) values ('oneindig');
 -- populate with data
 --
 -- generared using
@@ -47,21 +87,23 @@ CREATE TABLE products (
 --
 
 
-insert into products (name, description, code, price, rating_id) values ('God of War ', 'Vecht samen met Kratos en Atreus in Midgard', '816905633-0', 20, 1);
-insert into products (name, description, code, price, rating_id) values ('Dark Souls', 'Steek de eerste bonfire aan als de Ashen one', '077030122-3', 60,2);
-insert into products (name, description, code, price, rating_id) values ('Skate 3', 'skate time', '445924201-X', 5,2);
-insert into products (name, description, code, price, rating_id) values ('FIFA 22', 'voetbal time', '693155505-7', 60,3);
-insert into products (name, description, code, price, rating_id) values ('Minecraft', 'Mine en Craft in de wereld van Minecraft', '686928463-6', 25,1);
-insert into products (name, description, code, price, rating_id) values ('Destiny', 'Vermoord aliens met space magic', '492662523-7', 60,2);
+insert into products (name, description, code, price, rating_id) values ('God of War ', 'Vecht samen met Kratos en Atreus in Midgard.', '816905633-0', 20, 1);
+insert into products (name, description, code, price, rating_id) values ('Dark Souls', 'Steek de eerste bonfire aan als de Ashen one.', '077030122-3', 60,2);
+insert into products (name, description, code, price, rating_id) values ('Skate 3', 'Skate time.', '445924201-X', 5,2);
+insert into products (name, description, code, price, rating_id) values ('FIFA 22', 'Voetbal time.', '693155505-7', 60,3);
+insert into products (name, description, code, price, rating_id) values ('Minecraft', 'Mine en craft in de wereld van Minecraft.', '686928463-6', 25,1);
+insert into products (name, description, code, price, rating_id) values ('Destiny', 'Vermoord aliens met space magic.', '492662523-7', 60,2);
 
 insert into products (name, description, code, price, rating_id) values ('Farcry', 'Je vader is de koning van Kyrat en je kan kiezen of je Kyrat wilt bevrijden, of om samen met hem het land te verpesten.', '492662523-7', 30,1);
-insert into products (name, description, code, price, rating_id) values ('Spider-Man','pizza time.', '492662523-7', 20,1);
-insert into products (name, description, code, price, rating_id) values ('ARK', 'Overleef in de ruige wereld van ARK en vecht, of maak dinos tam', '492662523-7', 30,2);
-insert into products (name, description, code, price, rating_id) values ('F1 2021', 'Neem het op tegen Max Verstappe en zijn vrienden', '492662523-7', 5,3);
-insert into products (name, description, code, price, rating_id) values ('Persona 5 Royal', 'Overleef in de Japanse maatschappij en steel de schat van slechteriken', '492662523-7', 60, 1);
-insert into products (name, description, code, price, rating_id) values ('Rust', 'Overleer in de ruige wereld van Rust en vecht, of werk samen met naakte mensen', '492662523-7', 40, 4);  
-  insert into products (name, description, code, price, rating_id) values ('Assasins Creed Valhalla', 'Wees een assasin in Noorwegen', '492662523-7', 5, 4);
-  insert into products (name, description, code, price, rating_id) values ('Uncharted', 'Steel de schat van oude tempels, als Nathan Drake', '492662523-7', 60, 3);
-insert into products (name, description, code, price, rating_id) values ('Track Mania', 'Neem het op tegen andere racers', '492662523-7', 40,4);
+insert into products (name, description, code, price, rating_id) values ('Spider-Man','Pizza time.', '492662523-7', 20,1);
+insert into products (name, description, code, price, rating_id) values ('ARK', 'Overleef in de ruige wereld van ARK en vecht, of maak dinos tam.', '492662523-7', 30,2);
+insert into products (name, description, code, price, rating_id) values ('F1 2021', 'Neem het op tegen Max Verstappe.', '492662523-7', 5,3);
+insert into products (name, description, code, price, rating_id) values ('Persona 5 Royal', 'Overleef in de Japanse maatschappij en steel de schat van slechteriken.', '492662523-7', 60, 1);
+insert into products (name, description, code, price, rating_id) values ('Rust', 'Overleef in de ruige wereld van Rust en vecht, of werk samen met naakte mensen.', '492662523-7', 40, 4);  
+  insert into products (name, description, code, price, rating_id) values ('Assasins Creed Valhalla', 'Wees een assasin in Noorwegen.', '492662523-7', 5, 4);
+  insert into products (name, description, code, price, rating_id) values ('Uncharted', 'Steel de schat van oude tempels, als Nathan Drake.', '492662523-7', 60, 3);
+insert into products (name, description, code, price, rating_id) values ('Track Mania', 'Neem het op tegen andere racers.', '492662523-7', 40,4);
   insert into products (name, description, code, price, rating_id) values ('Horizon Zero Dawn', 'Neem het 
- op tegen robot dieren als Alloy', '492662523-7', 20, 4);
+ op tegen robot dieren als Alloy.', '492662523-7', 20, 4);
+
+  
